0049abd53b818afc5b98caabfacb5e70aab2e8d7
MMz <mmz.source@gmail.com>
2017-10-29 23:38:01 +0100

    cleanup + document + add particles

--- src/verlet/core.clj
+++ src/verlet/core.clj
@@ -6,11 +6,11 @@
 ;; Physics Simulation ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def height   500)
-(def width    500)
-(def bounce   0.9)
-(def gravity  0.5)
-(def friction 0.995)
+(def height   500)   ;; world height
+(def width    500)   ;; world width
+(def bounce   0.9)   ;; 10% velocity loss after hitting world border
+(def gravity  0.5)   ;; world gravity
+(def friction 0.995) ;; 0.5% velocity loss in every step
 
 (defn update-point [{:keys [x y oldx oldy]}]
   (let [vx    (* (- x oldx) friction)
@@ -20,9 +20,9 @@
       ;; Hit the floor
       (> y height) {:x (:x point) :y height :oldx (:oldx point) :oldy (+ height (* vy bounce))}
       ;; Hit the ceiling
-      (< y 0) {:x (:x point) :y 0 :oldx (:oldx point) :oldy (* vy bounce)}
+      (< y 0)      {:x (:x point) :y 0 :oldx (:oldx point) :oldy (* vy bounce)}
       ;; Hit the left wall
-      (< x 0) {:x 0 :y (:y point) :oldx (* vx bounce) :oldy (:oldy point)}
+      (< x 0)      {:x 0 :y (:y point) :oldx (* vx bounce) :oldy (:oldy point)}
       ;; Hit the right wall
       (> x width) {:x width :y (:y point) :oldx (+ width (* vx bounce)) :oldy (:oldy point)}
       ;; Free movement
@@ -43,8 +43,9 @@
 (defn setup []
   (quil/frame-rate  30)
   (quil/background 255)
-  (atom [{:x 3 :y 1 :oldx 0 :oldy 0}
-         {:x 100 :y 100 :oldx 100 :oldy 100}]))
+  (atom [{:x   3 :y   1 :oldx   0 :oldy    0}
+         {:x 100 :y 100 :oldx 100 :oldy  100}
+         {:x   0 :y 500 :oldx  -5 :oldy  524}]))
 
 
 (defn draw [points]
