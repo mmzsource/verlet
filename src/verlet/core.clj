6f2501fa4eb2f7dc943e018960207462bf1a0f9c
MMz <mmz.source@gmail.com>
2017-10-30 23:09:08 +0100

    connect 2 points with a stick

--- src/verlet/core.clj
+++ src/verlet/core.clj
@@ -17,6 +17,13 @@
   (reduce-kv (fn [m k v] (assoc m k (f v))) (empty coll) coll))
 
 
+(defn distance-map [p0 p1]
+  (let [dx (- (:x p1) (:x p0))
+        dy (- (:y p1) (:y p0))
+        distance (Math/sqrt (+ (* dx dx) (* dy dy)))]
+    {:dx dx :dy dy :distance distance}))
+
+
 (defn update-point [{:keys [x y oldx oldy]}]
   (let [vx (* (- x oldx) friction)
         vy (* (- y oldy) friction)]
@@ -28,8 +35,36 @@
   state)
 
 
+(defn calc-stick-constraint [stick p0 p1]
+  (let [distance-map (distance-map p0 p1)
+        difference   (- (:length stick) (:distance distance-map))
+        percentage   (/ (/ difference (:distance distance-map)) 2)
+        ;; percentage   (/ difference (/ (:distance distance-map) 2))
+        offsetX      (* (:dx distance-map) percentage)
+        offsetY      (* (:dy distance-map) percentage)
+        p0-new       {:x    (- (:x p0) offsetX)
+                      :y    (- (:y p0) offsetY)
+                      :oldx (:oldx p0)
+                      :oldy (:oldy p0)}
+        p1-new       {:x    (+ (:x p1) offsetX)
+                      :y    (+ (:y p1) offsetY)
+                      :oldx (:oldx p1)
+                      :oldy (:oldy p1)}]
+    [p0-new p1-new]))
+
+
+(defn apply-stick-constraint [state stick]
+  (let [p0-key     (first  (:links stick))
+        p0         (p0-key (:points @state))
+        p1-key     (last   (:links stick))
+        p1         (p1-key (:points @state))
+        new-points (calc-stick-constraint stick p0 p1)]
+    (swap! state assoc-in [:points p0-key] (first new-points))
+    (swap! state assoc-in [:points p1-key] (last  new-points))))
+
+
 (defn apply-stick-constraints [state]
-  (swap! state assoc :sticks (:sticks @state))
+  (apply-stick-constraint state (first (:sticks @state)))
   state)
 
 
@@ -70,17 +105,25 @@
 (defn setup []
   (quil/frame-rate  30)
   (quil/background 255)
-  (atom {:points {:p0 {:x   3 :y   1 :oldx   0 :oldy    0}
-                  :p1 {:x 100 :y 100 :oldx 100 :oldy  100}
-                  :p2 {:x   0 :y 500 :oldx  -5 :oldy  524}}
-         :sticks [{:links [:p0 :p1] :length "tbd: distance calculation"}]}))
+  (let [points {:p0 {:x   3 :y   1 :oldx   0 :oldy    0}
+                :p1 {:x 100 :y 100 :oldx 100 :oldy  100}
+                :p2 {:x   0 :y 500 :oldx  -5 :oldy  524}}
+        sticks [{:links  [:p0 :p1]
+                 :length (:distance (distance-map (:p0 points) (:p1 points)))}]]
+    (atom {:points points
+           :sticks sticks})))
 
 
 (defn draw [state]
   (quil/background 255)
   (quil/fill 0)
   (doseq [point (vals (:points @state))]
-    (quil/ellipse (:x point) (:y point) 7 7)))
+    (quil/ellipse (:x point) (:y point) 7 7))
+  (doseq [stick (:sticks @state)]
+    (let [points (:points @state)
+          p0     ((first (:links stick)) points)
+          p1     ((last  (:links stick)) points)]
+      (quil/line (:x p0) (:y p0) (:x p1) (:y p1)))))
 
 
 (quil/defsketch verlet
