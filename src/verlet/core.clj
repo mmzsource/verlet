f36793cbad5714130c84f85db90416c26b5da1bf
MMz <mmz.source@gmail.com>
2017-10-31 22:44:38 +0100

    Enable 'pinned' points

--- src/verlet/core.clj
+++ src/verlet/core.clj
@@ -24,10 +24,12 @@
     {:dx dx :dy dy :distance distance}))
 
 
-(defn update-point [{:keys [x y oldx oldy]}]
+(defn update-point [{:keys [x y oldx oldy pinned]}]
   (let [vx (* (- x oldx) friction)
         vy (* (- y oldy) friction)]
-    {:x (+ x vx) :y (+ y vy gravity) :oldx x :oldy y}))
+    (if pinned
+      {:x x :y y :oldx oldx :oldy oldy :pinned pinned}
+      {:x (+ x vx) :y (+ y vy gravity) :oldx x :oldy y})))
 
 
 (defn update-points [state]
@@ -39,18 +41,19 @@
   (let [distance-map (distance-map p0 p1)
         difference   (- (:length stick) (:distance distance-map))
         percentage   (/ (/ difference (:distance distance-map)) 2)
-        ;; percentage   (/ difference (/ (:distance distance-map) 2))
         offsetX      (* (:dx distance-map) percentage)
         offsetY      (* (:dy distance-map) percentage)
-        p0-new       {:x    (- (:x p0) offsetX)
-                      :y    (- (:y p0) offsetY)
-                      :oldx (:oldx p0)
-                      :oldy (:oldy p0)}
-        p1-new       {:x    (+ (:x p1) offsetX)
-                      :y    (+ (:y p1) offsetY)
-                      :oldx (:oldx p1)
-                      :oldy (:oldy p1)}]
-    [p0-new p1-new]))
+        p0-new       {:x      (- (:x p0) offsetX)
+                      :y      (- (:y p0) offsetY)
+                      :oldx   (:oldx p0)
+                      :oldy   (:oldy p0)
+                      :pinned (:pinned p0)}
+        p1-new       {:x      (+ (:x p1) offsetX)
+                      :y      (+ (:y p1) offsetY)
+                      :oldx   (:oldx p1)
+                      :oldy   (:oldy p1)
+                      :pinned (:pinned p1)}]
+    [(if (:pinned p0) p0 p0-new) (if (:pinned p1) p1 p1-new)]))
 
 
 (defn apply-stick-constraint [state stick]
@@ -64,22 +67,29 @@
 
 
 (defn apply-stick-constraints [state]
-  (apply-stick-constraint state (first (:sticks @state)))
+  (doseq [stick (:sticks @state)]
+    (let [p0-key     (first  (:links stick))
+          p0         (p0-key (:points @state))
+          p1-key     (last   (:links stick))
+          p1         (p1-key (:points @state))
+          new-points (calc-stick-constraint stick p0 p1)]
+      (swap! state assoc-in [:points p0-key] (first new-points))
+      (swap! state assoc-in [:points p1-key] (last  new-points))))
   state)
 
 
-(defn constrain-point [{:keys [x y oldx oldy] :as point}]
+(defn constrain-point [{:keys [x y oldx oldy pinned] :as point}]
    (let [vx (* (- x oldx) friction)
          vy (* (- y oldy) friction)]
     (cond
       ;; Hit the floor
-      (> y height) {:x x :y height :oldx oldx :oldy (+ height (* vy bounce))}
+      (> y height) {:x x :y height :oldx oldx :oldy (+ height (* vy bounce)) :pinned pinned}
       ;; Hit the ceiling
-      (< y 0)      {:x x :y 0 :oldx oldx :oldy (* vy bounce)}
+      (< y 0)      {:x x :y 0 :oldx oldx :oldy (* vy bounce) :pinned pinned}
       ;; Hit the left wall
-      (< x 0)      {:x 0 :y y :oldx (* vx bounce) :oldy oldy}
+      (< x 0)      {:x 0 :y y :oldx (* vx bounce) :oldy oldy :pinned pinned}
       ;; Hit the right wall
-      (> x width) {:x width :y y :oldx (+ width (* vx bounce)) :oldy oldy}
+      (> x width) {:x width :y y :oldx (+ width (* vx bounce)) :oldy oldy :pinned pinned}
       ;; Free movement
       :else point)))
 
@@ -105,11 +115,18 @@
 (defn setup []
   (quil/frame-rate  30)
   (quil/background 255)
-  (let [points {:p0 {:x   3 :y   1 :oldx   0 :oldy    0}
-                :p1 {:x 100 :y 100 :oldx 100 :oldy  100}
-                :p2 {:x   0 :y 500 :oldx  -5 :oldy  524}}
+  (let [points {:p0 {:x   3 :y   1 :oldx   0 :oldy   0}
+                :p1 {:x 100 :y 100 :oldx 100 :oldy 100}
+                :p2 {:x   0 :y 500 :oldx  -5 :oldy 524}
+                :p3 {:x  25 :y 475 :oldx  -5 :oldy 525}
+                :p4 {:x 250 :y  50 :oldx 250 :oldy 50 :pinned true}
+                :p5 {:x 299 :y   1 :oldx 299 :oldy  1}}
         sticks [{:links  [:p0 :p1]
-                 :length (:distance (distance-map (:p0 points) (:p1 points)))}]]
+                 :length (:distance (distance-map (:p0 points) (:p1 points)))}
+                {:links  [:p2 :p3]
+                 :length (:distance (distance-map (:p2 points) (:p3 points)))}
+                {:links  [:p4 :p5]
+                 :length 50}]]
     (atom {:points points
            :sticks sticks})))
 
